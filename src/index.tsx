9772815498905933c91eea857f09038827abd94d
Nicolas Scozzaro <scozzaro@uber.com>
2025-02-15 19:38:51 +0000

    Added redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
-// index.tsx
+// src/index.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import App from './App';
 import './App.css';
+import { store } from './store';
 
 const container = document.getElementById('root');
 
 if (container) {
   const root = createRoot(container);
-
   root.render(
     <React.StrictMode>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-      </DndProvider>
+      <Provider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+        </DndProvider>
+      </Provider>
     </React.StrictMode>
   );
 } else {
