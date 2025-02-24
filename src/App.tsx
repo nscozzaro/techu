4c1b730807e92ab23cc2ebd4a0e26e72613f6600
Nicolas Scozzaro <scozzaro@uber.com>
2025-02-24 08:43:00 +0000

    Consolidate useEffect in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Board from './components/Board';
@@ -12,30 +13,25 @@ import { selectScores } from './selectors';
 function App() {
   const players = useSelector((state: RootState) => state.game.players);
   const currentTurn = useSelector((state: RootState) => state.game.turn.currentTurn);
-  const { initialFaceDownCards, gameOver } = useSelector(
-    (state: RootState) => state.game.gameStatus
-  );
+  const { initialFaceDownCards, gameOver } = useSelector((state: RootState) => state.game.gameStatus);
   const scores = useSelector(selectScores);
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
+    // Flip initial face-down cards if both players have one
     if (initialFaceDownCards[PlayerEnum.PLAYER1] && initialFaceDownCards[PlayerEnum.PLAYER2]) {
       dispatch(setHighlightedCells([]));
       dispatch(flipInitialCards());
     }
-  }, [initialFaceDownCards, dispatch]);
-
-  useEffect(() => {
+    // If it's Player2's turn and the game isn't over, play their turn
     if (currentTurn === PlayerEnum.PLAYER2 && !gameOver) {
       dispatch(playTurn(PlayerEnum.PLAYER2));
     }
-  }, [currentTurn, gameOver, dispatch]);
-
-  useEffect(() => {
+    // Check if the game is over
     if (isGameOver(players)) {
       dispatch(setGameOver(true));
     }
-  }, [players, dispatch]);
+  }, [initialFaceDownCards, currentTurn, gameOver, players, dispatch]);
 
   return (
     <div className="App">
