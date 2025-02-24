e6c0c68b8d7e7433ce748ea1043e1eb6c88694c1
Nicolas Scozzaro <scozzaro@uber.com>
2025-02-24 08:37:37 +0000

    React.Fragment

--- src/components/PlayerArea.tsx
+++ src/components/PlayerArea.tsx
@@ -1,12 +1,10 @@
+// src/components/PlayerArea.tsx
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, AppDispatch } from '../store';
 import Cell from './Cell';
 import { PlayerEnum } from '../types';
-import {
-  setDraggingPlayer,
-  resetUI,
-} from '../features/game';
+import { setDraggingPlayer, resetUI } from '../features/game';
 import { selectHandForPlayer, selectDeckCountForPlayer } from '../selectors';
 
 interface PlayerAreaProps {
@@ -16,57 +14,48 @@ interface PlayerAreaProps {
 const PlayerArea: React.FC<PlayerAreaProps> = ({ playerId }) => {
   const dispatch = useDispatch<AppDispatch>();
 
-  // Get what we need from Redux:
-  const {
-    discardPile,
-    firstMove,
-    currentTurn,
-    highlightedCells,
-    highlightDiscardPile,
-  } = useSelector((state: RootState) => ({
-    discardPile: state.game.discard[playerId],
-    firstMove: state.game.gameStatus.firstMove[playerId],
-    currentTurn: state.game.turn.currentTurn,
-    highlightedCells: state.game.highlightedCells,
-    highlightDiscardPile: state.game.highlightDiscardPile,
-  }));
+  const { discardPile, firstMove, currentTurn, highlightedCells, highlightDiscardPile } =
+    useSelector((state: RootState) => ({
+      discardPile: state.game.discard[playerId],
+      firstMove: state.game.gameStatus.firstMove[playerId],
+      currentTurn: state.game.turn.currentTurn,
+      highlightedCells: state.game.highlightedCells,
+      highlightDiscardPile: state.game.highlightDiscardPile,
+    }));
 
-  // Use our selectors:
-  const deckCount = useSelector((state: RootState) =>
-    selectDeckCountForPlayer(state, playerId)
-  );
-  const handCards = useSelector((state: RootState) =>
-    selectHandForPlayer(state, playerId)
-  );
-
-  // Removed clearHighlights entirely; we now clear highlights directly in the drag-drop hook
+  const deckCount = useSelector((state: RootState) => selectDeckCountForPlayer(state, playerId));
+  const handCards = useSelector((state: RootState) => selectHandForPlayer(state, playerId));
 
   const handleDragStart = () => dispatch(setDraggingPlayer(playerId));
   const handleDragEnd = () => dispatch(resetUI());
 
-  const renderDeck = () => (
-    <Cell
-      type="deck"
-      count={deckCount}
-      playerId={playerId}
-      isCurrentPlayer={currentTurn === playerId}
-    />
-  );
-
-  const renderDiscard = () => (
-    <Cell
-      type="discard"
-      stack={discardPile}
-      playerId={playerId}
-      isVisible={true}
-      isCurrentPlayer={currentTurn === playerId}
-      isDisabled={firstMove}
-      isHighlighted={currentTurn === playerId ? highlightDiscardPile : false}
-    />
-  );
-
-  const renderHand = () => {
-    return handCards.map((card, index: number) => (
+  // Consolidate cell rendering based on type.
+  const renderCell = (type: 'deck' | 'hand' | 'discard') => {
+    if (type === 'deck') {
+      return (
+        <Cell
+          type="deck"
+          count={deckCount}
+          playerId={playerId}
+          isCurrentPlayer={currentTurn === playerId}
+        />
+      );
+    }
+    if (type === 'discard') {
+      return (
+        <Cell
+          type="discard"
+          stack={discardPile}
+          playerId={playerId}
+          isVisible={true}
+          isCurrentPlayer={currentTurn === playerId}
+          isDisabled={firstMove}
+          isHighlighted={currentTurn === playerId ? highlightDiscardPile : false}
+        />
+      );
+    }
+    // Render hand cells.
+    return handCards.map((card, index) => (
       <Cell
         key={index}
         type="hand"
@@ -81,21 +70,15 @@ const PlayerArea: React.FC<PlayerAreaProps> = ({ playerId }) => {
     ));
   };
 
+  // For Player1 the order is deck, hand, discard; for Player2 it's reversed for deck and discard.
+  const order: Array<'deck' | 'hand' | 'discard'> =
+    playerId === PlayerEnum.PLAYER1 ? ['deck', 'hand', 'discard'] : ['discard', 'hand', 'deck'];
+
   return (
     <div className="player-area">
-      {playerId === PlayerEnum.PLAYER1 ? (
-        <>
-          {renderDeck()}
-          {renderHand()}
-          {renderDiscard()}
-        </>
-      ) : (
-        <>
-          {renderDiscard()}
-          {renderHand()}
-          {renderDeck()}
-        </>
-      )}
+      {order.map((type) => (
+        <React.Fragment key={type}>{renderCell(type)}</React.Fragment>
+      ))}
     </div>
   );
 };
